bfb55628e889a2c3dafe49f15ac47fe5238407bc
MingMing-28 <139741841+MingMing-28@users.noreply.github.com>
2023-07-21 15:36:26 +0800

    Assignment

--- src/style/index.js
+++ src/style/index.js
@@ -1,13 +1,51 @@
 import React from "react";
-
 import ReactDOM from "react-dom";
-
+import './style/index.css'
 const App = () => {
   return (
     <>
-    <div> Color Red
-      
-    </div>
+      <body>
+        <div class="grid-container">
+            <div class="login-container">
+                <h2>Login</h2>
+                <form action="/login" method="post">
+                    <div class="login">
+                        <div class="group">
+                            <label for="username">Username:</label>
+                            <br />
+                            <input type="text" name="username" value="Type your username"/>
+                        </div>
+                        <div class="group">
+                            <label for="password">Password:</label>
+                            <br />
+                            <input type="text" name="password" value="Type your password"/>
+                        </div>
+                        <div class="group">
+                             <input type="checkbox" name="remember"/> 
+                             <label for="remember">Remember me</label>
+                        </div>
+                       
+                        <div class="group"> 
+                            <input type="submit" value="LOGIN"/> 
+                        </div>
+                       
+                    </div>
+                </form>
+            </div>        
+            <div class="steps">
+                <h4>Steps</h4>
+                <ul>
+                    <li>Enter your username</li>
+                    <li>Enter your password
+                        <ul>
+                            <li>Click Remember me to save your password</li>
+                        </ul>
+                    </li>
+                    <li>Click Login</li>
+                </ul>
+            </div>
+        </div>  
+    </body>
     </>
   )
 };
